aa28cbb76ea1398d9a77a684c37fb2a2b487d5e1
savannahmarshall <savvymarshall@gmail.com>
2024-10-02 18:56:06 -0700

    fixed issue with auth not working

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const withAuth = require('../utils/auth'); 
+
 
 // route for dashboard 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', withAuth, (req, res) => {
     res.render('dashboard'); 
 });
 
