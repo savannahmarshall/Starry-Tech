dade78009044df19ef6e77a8fce19364e2d8eafb
savannahmarshall <savvymarshall@gmail.com>
2024-10-04 19:26:16 -0700

    fixed issue with dashboard not rendering when user is logged in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,34 +15,29 @@ router.get('/', async (req, res) => {
 // Route for the dashboard
 router.get('/dashboard', withAuth, async (req, res) => {
   try {
+    // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{ model: Post }],
     });
 
-    // Check if userData is found
-    if (!userData) {
-      console.log('User not found for dashboard, redirecting to login.');
-      return res.redirect('/login'); 
-    }
-
     const user = userData.get({ plain: true });
 
     res.render('dashboard', {
       ...user,
-      logged_in: true,
+      logged_in: true
     });
   } catch (err) {
-    console.error('Error fetching dashboard data:', err);
     res.status(500).json(err);
   }
 });
+
 
 // Route for the login page
 router.get('/login', (req, res) => {
   console.log('Login page accessed. Logged in:', req.session.logged_in);
   if (req.session.logged_in) {
-    // return res.redirect('/dashboard');
+    return res.redirect('/dashboard'); 
   }
   res.render('login', { logged_in: req.session.logged_in });
 });
